dd2a20ebf33391c0c4a2be25b8e4ada01944deb7
pdharma <pdharma@intertrust.com>
2015-07-18 21:01:15 -0700

    Add tab separated output format

---
+++ camus-kafka-coders/src/main/java/com/linkedin/camus/etl/kafka/coders/StringMessageDecoder.java
@@ -0,0 +1,43 @@
+package com.linkedin.camus.etl.kafka.coders;
+
+import com.linkedin.camus.coders.CamusWrapper;
+import com.linkedin.camus.coders.Message;
+import com.linkedin.camus.coders.MessageDecoder;
+import org.apache.log4j.Logger;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+
+/**
+ * MessageDecoder class that will convert the payload into a String object,
+ * System.currentTimeMillis() will be used to set CamusWrapper's
+ * timestamp property
+
+ * This MessageDecoder returns a CamusWrapper that works with Strings payloads,
+ */
+public class StringMessageDecoder extends MessageDecoder<Message, String> {
+  private static final Logger log = Logger.getLogger(StringMessageDecoder.class);
+
+  @Override
+  public void init(Properties props, String topicName) {
+    this.props = props;
+    this.topicName = topicName;
+  }
+
+  @Override
+  public CamusWrapper<String> decode(Message message) {
+    long timestamp = 0;
+    String payloadString;
+
+    try {
+      payloadString = new String(message.getPayload(), "UTF-8");
+    } catch (UnsupportedEncodingException e) {
+      log.error("Unable to load UTF-8 encoding, falling back to system default", e);
+      payloadString = new String(message.getPayload());
+    }
+    timestamp = System.currentTimeMillis();
+
+    return new CamusWrapper<String>(payloadString, timestamp);
+  }
+}
